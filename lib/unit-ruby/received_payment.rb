0abeb87c1ca21f826a0d89b76e13433e15086c03
grant-thatch <grant@thatch.ai>
2025-04-11 09:54:06 -0400

    [FED-900] Add Find to ReceivedPayment (#27)
    * [FED-900] Add Find to ReceivedPayment
    
    * [FED-900] Switch to correct file
    
    * [FED-900] Fix attribute naming to sanke case

--- lib/unit-ruby/received_payment.rb
+++ lib/unit-ruby/received_payment.rb
@@ -4,22 +4,25 @@ module Unit
 
     attribute :created_at, Types::DateTime, readonly: true
     attribute :status, Types::String, readonly: true
-    attribute :wasAdvanced, Types::Boolean, readonly: true
+    attribute :was_advanced, Types::Boolean, readonly: true
     attribute :amount, Types::Integer, readonly: true
-    attribute :completionDate, Types::Date, readonly: true
-    attribute :companyName, Types::String, readonly: true
-    attribute :counterpartyRoutingNumber, Types::String, readonly: true
+    attribute :completion_date, Types::Date, readonly: true
+    attribute :company_name, Types::String, readonly: true
+    attribute :counterparty_routing_number, Types::String, readonly: true
     attribute :description, Types::String, readonly: true
-    attribute :traceNumber, Types::String, readonly: true
-    attribute :secCode, Types::String, readonly: true
-    attribute :returnCutoffTime, Types::DateTime, readonly: true
-    attribute :canBeReprocessed, Types::Boolean, readonly: true
+    attribute :trace_number, Types::String, readonly: true
+    attribute :sec_code, Types::String, readonly: true
+    attribute :return_cutoff_time, Types::DateTime, readonly: true
+    attribute :can_be_reprocessed, Types::Boolean, readonly: true
     attribute :tags, Types::Hash, readonly: true
 
     belongs_to :account, class_name: 'Unit::DepositAccount'
     belongs_to :customer, class_name: 'Unit::IndividualCustomer' # Optional
-    belongs_to :receivePaymentTransaction, class_name: 'Unit::Transaction' # Optional
-    belongs_to :paymentAdvanceTransaction, class_name: 'Unit::Transaction' # Optional
-    belongs_to :repayPaymentAdvanceTransaction, class_name: 'Unit::Transaction' # Optional
+    belongs_to :receive_payment_transaction, class_name: 'Unit::Transaction' # Optional
+    belongs_to :payment_advance_transaction, class_name: 'Unit::Transaction' # Optional
+    belongs_to :repay_payment_advance_transaction, class_name: 'Unit::Transaction' # Optional
+
+    include ResourceOperations::Find
+    include ResourceOperations::List
   end
 end
